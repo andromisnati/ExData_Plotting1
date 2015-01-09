bd4baa3ae171d7c82629c6d68bce80cd8a4b6bc5
andromisnati <andromisnati@gmail.com>
2015-01-09 13:45:45 -0500

    Save png plot 2 & 3 (post-pain to make the legend stop moving)

--- plot2.R
+++ plot2.R
@@ -1,17 +1,22 @@
 #setwd("../Downloads/exploratory-analysis/git/ExData_Plotting1")
 Sys.setlocale("LC_TIME", "English")
-#or we have Spanish Days
+#Set ENG language or we'll have Spanish Days in the plot XD
 
+#Read columns names
 nombres<-read.table("household_power_consumption.txt",sep=";",
                     nrows=1,header=TRUE)
 cols<-colnames(nombres)
+#Read needed data
 mydata<-read.table("household_power_consumption.txt",sep=";",
                    skip=66637,na.strings="?",nrows=2880)
 names(mydata)<-cols
 mydata$Hora<-strptime(paste(mydata$Time,mydata$Date,sep="-"),"%H:%M:%S-%d/%m/%Y")
-#mydata$Fecha<-as.Date(mydata$Date,"%d/%m/%Y")
 
-#Data read with labels
-
+#Data read with labels, time objects, now create plot
 plot(mydata$Hora,mydata$Global_active_power,main="",type="l",
      ylab="Global Active Power (kilowatts)",xlab="")
+
+#save to png
+dev.copy(png, file = "plot2.png",width=480,height=480) 
+#close
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,21 +1,28 @@
 #setwd("../Downloads/exploratory-analysis/git/ExData_Plotting1")
 Sys.setlocale("LC_TIME", "English")
-#or we have Spanish Days
+#Set ENG language or we'll have Spanish Days in the plot XD
 
+#Read columns names
 nombres<-read.table("household_power_consumption.txt",sep=";",
                     nrows=1,header=TRUE)
 cols<-colnames(nombres)
+#Read needed data
 mydata<-read.table("household_power_consumption.txt",sep=";",
                    skip=66637,na.strings="?",nrows=2880)
 names(mydata)<-cols
 mydata$Hora<-strptime(paste(mydata$Time,mydata$Date,sep="-"),"%H:%M:%S-%d/%m/%Y")
-#mydata$Fecha<-as.Date(mydata$Date,"%d/%m/%Y")
 
-#Data read with labels
+#Data read with labels, time objects, now create plot
 
 plot(mydata$Hora,mydata$Sub_metering_1,main="",type="l",
      ylab="Energy sub metering",xlab="")
 lines(mydata$Hora,mydata$Sub_metering_2,col="red")
 lines(mydata$Hora,mydata$Sub_metering_3,col="blue")
 legend("topright", pch = "-", col = c("black", "red","blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+       legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),
+       text.width=strwidth("- Sub_metering_1_____")  )
+
+#save to png
+dev.copy(png, file = "plot3.png",width=480,height=480) 
+#close
+dev.off()
